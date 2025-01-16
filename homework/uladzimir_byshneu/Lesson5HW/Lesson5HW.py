796cf0687a1ee29e0a72d04e2dc34b3fb389cd2c
Byshnev Uladzimir <byshnevvladimir@gmail.com>
2025-01-16 11:48:14 +0300

    Lesson5HW 2 try

--- homework/uladzimir_byshneu/Lesson5HW/Lesson5HW.py
+++ homework/uladzimir_byshneu/Lesson5HW/Lesson5HW.py
@@ -5,17 +5,17 @@ name, last_name, city, phone, country = person
 line1 = "результат операции: 42"
 line2 = "результат операции: 514"
 line3 = "результат работы программы: 9"
-# Обработка первой строки
+# Обработка первой строки.
 colon_index1 = line1.index(':')
 number1 = int(line1[colon_index1 + 1:])
 result1 = number1 + 10
 print(result1)
-# Обработка второй строки
+# Обработка второй строки.
 colon_index2 = line2.index(':')
 number2 = int(line2[colon_index2 + 1:])
 result2 = number2 + 10
 print(result2)
-# Обработка третьей строки
+# Обработка третьей строки.
 colon_index3 = line3.index(':')
 number3 = int(line3[colon_index3 + 1:])
 result3 = number3 + 10
