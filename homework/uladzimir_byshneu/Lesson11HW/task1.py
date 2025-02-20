3f3f5270312e15873358c95689f760b0727f9e70
Byshnev Uladzimir <byshnevvladimir@gmail.com>
2025-02-20 12:25:54 +0300

    Lesson 11 HW 2 try

--- homework/uladzimir_byshneu/Lesson11HW/task1.py
+++ homework/uladzimir_byshneu/Lesson11HW/task1.py
@@ -10,16 +10,11 @@ class Book:
         self.book_reservation = book_reservation
 
 
-book_1 = Book('Атлант расправил плечи', "А.Рэнд", 1408,
-              'ISBN 978-5-9614-6742-0', True)
-book_2 = Book('Empire V', 'В. Пелевин', 267,
-              'ISBN 978-5-389-10814-1', False)
-book_3 = Book('Отцы и дети', 'И. Тургенев', 400,
-              'ISBN 978-5-17-089255-6', True)
-book_4 = Book("Герой нашего времени", "М. Лермонтов", 333,
-              'ISBN 978-5-389-24394-1', True)
-book_5 = Book("Жизнь взаймы","Э. Ремарк", 245,
-              'ISBN 978-5-17-112096-2', False)
+book_1 = Book('Атлант расправил плечи', "А.Рэнд", 1408, 'ISBN 978-5-9614-6742-0', True)
+book_2 = Book('Empire V', 'В. Пелевин', 267, 'ISBN 978-5-389-10814-1', False)
+book_3 = Book('Отцы и дети', 'И. Тургенев', 400, 'ISBN 978-5-17-089255-6', True)
+book_4 = Book("Герой нашего времени", "М. Лермонтов", 333, 'ISBN 978-5-389-24394-1', True)
+book_5 = Book("Жизнь взаймы", "Э. Ремарк", 245, 'ISBN 978-5-17-112096-2', False)
 
 books = []
 books.append(book_1)
@@ -44,15 +39,9 @@ class SchoolBook(Book):
         self.task_available = task_available
 
 
-guide_1 = SchoolBook('Химия 9-й класс', 'Гурченко', 345,
-                     '', True,
-                      'Химия', '9', True)
-guide_2 = SchoolBook('Русская литература 7-й класс', 'Пушкин', 368,
-                     '', False,
-                      'Русская литература',  '7', True)
-guide_3 = SchoolBook('Английский язык 8-й класс', 'Микашевич', 140,
-                     '', False,
-                      'Иностранный язык', '8', True)
+guide_1 = SchoolBook('Химия 9-й класс', 'Гурченко', 345, '', True, 'Химия', '9', True)
+guide_2 = SchoolBook('Русская литература 7-й класс', 'Пушкин', 368, '', False, 'Русская литература', '7', True)
+guide_3 = SchoolBook('Английский язык 8-й класс', 'Микашевич', 140, '', False, 'Иностранный язык', '8', True)
 
 guides = []
 guides.append(guide_1)
